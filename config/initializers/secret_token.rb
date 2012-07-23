cca081632fb295bf89161d32b7a34ddff79d8737
Calvin Tee <calvin@calvin-desktop.(none)>
2012-07-23 21:30:31 +0800

    initialize rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ctedit::Application.config.secret_token = '84c0f7db0e3a627bef90e1d718a18a4fd986371f2217521b33af994517c1b891fc9b37a02080fbaf4b5cf836d0c6e4b07fdd1ca666add2dfa6767a5c83dede8b'
